ae913bbfc49c34ecddb750a586f32550880a339c
Thanadet Manichote <thanadet.man@ascendcorp.com>
2018-03-30 16:23:24 +0700

    first commit

---
+++ src/config/help.php
@@ -0,0 +1,126 @@
+<?php
+
+return [
+    'method_list' => ['login', 'getMenuList', 'getMenuDetail', 'createMenu', 'updateMenu', 'deleteMenu', 'changePassword'],
+    'login' => [
+        'params' => [
+            'username' => 'required',
+            'password' => 'required',
+            'ref_type' => 'required',
+        ],
+        'output' => [
+            'attributes.username'   => 'Username',
+            'attributes.ref_type'   => 'Reference Type',
+            'attributes.ref_id'     => 'Reference ID',
+            'attributes.status'     => 'User Status',
+            'attributes.created_at' => 'Create date time',
+            'attributes.updated_at' => 'Update date time',
+            'id'                    => 'User ID',
+        ]
+    ],
+    'getUserList' => [
+        'params' => [
+            'username' => 'not required',
+            'ref_type' => 'not required',
+            'ref_id'   => 'not required',
+            'limit'    => 'not required',
+            'offset'   => 'not required',
+            'order_by' => 'not required (ex => username:asc)',
+        ],
+        'output' => [
+            [
+                'attributes.username'   => 'Username',
+                'attributes.ref_type'   => 'Reference Type',
+                'attributes.ref_id'     => 'Reference ID',
+                'attributes.status'     => 'User Status',
+                'attributes.created_at' => 'Create date time',
+                'attributes.updated_at' => 'Update date time',
+                'id'                    => 'User ID',
+            ]
+        ]
+    ],
+    'getUserList' => [
+        'params' => [],
+        'output' => [
+            'attributes.username'   => 'Username',
+            'attributes.ref_type'   => 'Reference Type',
+            'attributes.ref_id'     => 'Reference ID',
+            'attributes.status'     => 'User Status',
+            'attributes.created_at' => 'Create date time',
+            'attributes.updated_at' => 'Update date time',
+            'id'                    => 'User ID',
+        ]
+    ],
+    'createUser' => [
+        'params' => [
+            'username' => 'required',
+            'password' => 'required',
+            'ref_type' => 'required',
+            'status'   => 'required (active/inactive)',
+        ],
+        'output' => [
+            'attributes.username'   => 'Username',
+            'attributes.password'   => 'Password',
+            'attributes.ref_type'   => 'Reference Type',
+            'attributes.ref_id'     => 'Reference ID',
+            'attributes.created_at' => 'Create date time',
+            'attributes.updated_at' => 'Update date time',
+            'id'                    => 'User ID',
+        ]
+    ],
+    'updateUser' => [
+        'params' => [
+            'id'       => 'required'
+        ],
+        'output' => [
+            'attributes.username'   => 'Username',
+            'attributes.password'   => 'Password',
+            'attributes.ref_type'   => 'Reference Type',
+            'attributes.ref_id'     => 'Reference ID',
+            'attributes.created_at' => 'Create date time',
+            'attributes.updated_at' => 'Update date time',
+            'id'                    => 'User ID',
+        ]
+    ],
+    'deleteUser' => [
+        'params' => [
+            'id'       => 'required'
+        ],
+        'output' => [
+            'attributes.username'   => 'Username',
+            'attributes.password'   => 'Password',
+            'attributes.ref_type'   => 'Reference Type',
+            'attributes.ref_id'     => 'Reference ID',
+            'attributes.created_at' => 'Create date time',
+            'attributes.updated_at' => 'Update date time',
+            'id'                    => 'User ID',
+        ]
+    ],
+    'changePassword' => [
+        'params' => [
+            'old' => 'required',
+            'new' => 'required',
+        ],
+        'output' => [
+            'attributes.username'   => 'Username',
+            'attributes.password'   => 'Password',
+            'attributes.ref_type'   => 'Reference Type',
+            'attributes.ref_id'     => 'Reference ID',
+            'attributes.created_at' => 'Create date time',
+            'attributes.updated_at' => 'Update date time',
+            'id'                    => 'User ID',
+        ]
+    ],
+    'configs' => [
+        'user' => [
+            'url'             => 'http://ms-user-api.develop/',
+            'login'           => 'users/login',
+            'list'            => 'users',
+            'detail'          => 'users/[id]',
+            'create'          => 'users',
+            'update'          => 'users/[id]',
+            'delete'          => 'users/[id]',
+            'change_password' => 'users/[id]/change/password',
+        ],
+    ]
+];
